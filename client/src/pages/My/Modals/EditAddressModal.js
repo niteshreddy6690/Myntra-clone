71c40c9a28dbe0bcc83257479c261630a6a554a0
Nitesh <niteshreddy6690@gmail.com>
2024-08-07 01:20:28 +0530

    remove some space

--- client/src/pages/My/Modals/EditAddressModal.js
+++ client/src/pages/My/Modals/EditAddressModal.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
-import { DevTool } from "@hookform/devtools";
 
 const EditContainerModalWrapper = styled.div`
   background: rgba(0, 0, 0, 0.69);
